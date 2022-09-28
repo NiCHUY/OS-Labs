20c196c14900a99046a61461aa80604d417471eb
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2022-09-28 15:13:27 +0300

    Add files via upload

---
+++ Scripts/WindowsScript/WindowsScript.ps1
@@ -0,0 +1,12 @@
+﻿    # Number
+	g++ -c -o Number\Number.o Number\Number.cpp
+	ar rvs Number\Number.lib Number\Number.o
+	
+	# Vector
+	g++ -c -DVECTOR_EXPORTS -o Dll\Vector.o -INumber\ DllVector\MyVector.cpp
+	g++ -shared -o Vector\Vector.dll Vector\Vector.o Number\Number.lib
+	
+    # Main
+	g++ -o WindowsScript -INumber\ -IVector\ Sourse.cpp -L.\Number\ -L.\Vector\ -lNumber -lVector
+
+    .\WindowsScript.exe
